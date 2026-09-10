0de0fcf2b170910107031052550bfdee8131a883
Мозенков Егор Алексеевич <mozenkov.ea@phystech.edu>
2026-09-10 21:50:25 +0300

    Реализация LRU-кэша

---
+++ I_cache.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <list>
+#include <unordered_map>
+#include <string>
+
+class ICache {
+    public:
+
+    virtual ~ICache () = default;
+
+}
+
+class LRU_Cache {
+    private:
+    
+}
